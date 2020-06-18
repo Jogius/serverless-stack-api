ef8c7a346d188acf90f59dab98958c67841aaa38
Jogius <julius@themakowskis.de>
2020-06-18 16:46:56 +0200

    Adding our Serverless API

---
+++ delete.js
@@ -0,0 +1,19 @@
+import handler from "./libs/handler-lib";
+import dynamoDb from "./libs/dynamodb-lib";
+
+export const main = handler(async (event, context) => {
+    const params = {
+        TableName: process.env.tableName,
+        // 'Key' defines the partition and sort key of the item to be removed
+        // - 'userId': Identity Pool identity if od the authenticated user
+        // - 'noteId': path parameter
+        Key: {
+            userId: event.requestContext.identity.cognitoIdentityId,
+            noteId: event.pathParameters.id
+        }
+    };
+
+    await dynamoDb.delete(params);
+
+    return { status: true };
+});
