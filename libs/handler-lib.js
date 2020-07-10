9bc86a3b87c202bf7b8ed8dc648c902b243e058a
Jogius <julius@themakowskis.de>
2020-07-10 18:22:16 +0200

    Fixed typing mistake in serverless.yml, now fully functioning

--- libs/handler-lib.js
+++ libs/handler-lib.js
@@ -1,23 +1,24 @@
 export default function handler(lambda) {
-    return function (event, context) {
-        return Promise.resolve()
-        // Run the Lambda
-            .then(() => lambda(event, context))
-        // On success
-            .then((responseBody) => [200, responseBody])
-        // On failure
-            .catch((e) => {
-                console.log(e);
-                return [500, { error: e.message }];
-            })
+    return async function (event, context) {
+        let body, statusCode;
+
+        try {
+            // Run the lambda
+            body = await lambda(event, context);
+            statusCode = 200;
+        } catch (e) {
+            body = { error: e.message };
+            statusCode = 500;
+        }
+
         // Return HTTP response
-            .then(([statusCode, body]) => ({
-                statusCode,
-                headers: {
-                    "Access-Control-Allow-Origin": "*",
-                    "Access-Control-Allow-Credentials": true,
-                },
-                body: JSON.stringify(body),
-            }));
+        return {
+            statusCode,
+            body: JSON.stringify(body),
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Credentials": true,
+            },
+        };
     };
 }
